db732fba10aa1b45b7e06a656014111c35e0b3c4
tsk-balaji <tskbalaji15@gmail.com>
2024-10-29 19:42:28 +0530

    redux changes reverted

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import { useEffect } from "react";
+/* eslint-disable react-refresh/only-export-components */
+import { useLayoutEffect, useState, createContext } from "react";
 import "./App.css";
 import ProductContainer from "./Components/ProductContainer";
 import Total from "./Components/Total";
-import { useDispatch } from "react-redux";
-import { jsonSetProductsData } from "./redux/reducers/products.reducers";
+
+//Context API to pass the fetched json Data
+export const jsonData = createContext();
+
+// Create the SubtotalContext
+export const SubtotalContext = createContext();
 
 export default function App() {
-  const dispatcher = useDispatch();
+  const [products, setProducts] = useState([]);
+  const [subtotals, setSubtotals] = useState([]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     fetch("/products.json")
       .then((response) => response.json())
       .then((result) => {
-        dispatcher(jsonSetProductsData(result));
+        if (result.products.length > 0) {
+          setProducts(result.products);
+        }
       })
       .catch((error) => console.log(error));
-  }, []); // Adding [] to run this effect only once on mount
+  }, []);
 
   return (
-    <>
-      <ProductContainer />
-      <Total />
-    </>
+    <jsonData.Provider value={products}>
+      {/* Provide both subtotals and setSubtotals as part of the SubtotalContext */}
+      <SubtotalContext.Provider value={{ subtotals, setSubtotals }}>
+        <ProductContainer />
+        <Total />
+      </SubtotalContext.Provider>
+    </jsonData.Provider>
   );
 }
